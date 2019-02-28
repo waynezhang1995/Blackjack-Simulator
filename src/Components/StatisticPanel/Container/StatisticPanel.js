2c75159881977d32914c3cc179d1aecf82d1b695
Wayne Zhang <waynezhangyuwei@Gmail.com>
2019-02-27 20:57:29 -0800

    more tsx conversion

--- src/Components/StatisticPanel/Container/StatisticPanel.js
+++ src/Components/StatisticPanel/Container/StatisticPanel.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import StatisticPanel from '../Presentational/StatisticPanel.js';
+import StatisticPanel from '../Presentational/StatisticPanel';
 
 const mapStateToProps = (state, ownProps) => ({
     ...ownProps,
--- src/Components/StatisticPanel/Presentational/StatisticPanel.tsx
+++ src/Components/StatisticPanel/Presentational/StatisticPanel.tsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import "./StatisticPanel.css"
+import './StatisticPanel.css';
+
+export interface StatisticPanelProps {
+    dealerWinCount: Number;
+    playerWinCount: Number;
+    tieCount: Number;
+    remainingCards: String;
+    endShoeSummary: boolean;
+}
 
 const css = {
     statisticPanelBody: 'statistic-panel-body',
@@ -9,22 +17,22 @@ const css = {
     statisticPanelTitle: 'statistic-panel-title'
 };
 
-class StatisticPanel extends Component {
+class StatisticPanel extends Component<StatisticPanelProps, {}> {
     render() {
         const { dealerWinCount, playerWinCount, tieCount, remainingCards, endShoeSummary } = this.props;
         const panelContent = (
             <div>
-                <Typography className={css.statisticPanelTitle} variant="title" color="inherit">
+                <Typography className={css.statisticPanelTitle} variant='title' color='inherit'>
                     Game Statistic:
                 </Typography>
-                <Typography className={css.statisticPanelTitle} variant="caption" color="inherit">
+                <Typography className={css.statisticPanelTitle} variant='caption' color='inherit'>
                     (Data will be updated at the end of each round)
                 </Typography>
             </div>
         );
         const endGamePanelContent = (
             <div>
-                <Typography className={css.statisticPanelTitle} variant="title" color="inherit">
+                <Typography className={css.statisticPanelTitle} variant='title' color='inherit'>
                     End Shoe Statistic:
                 </Typography>
             </div>
